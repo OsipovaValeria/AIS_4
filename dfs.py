4ee2e2d4bee4df48822eedb9f65a52312de8c6fd
Valeria Osipova <osipova.val.21@gmail.com>
2024-04-18 22:51:00 +0300

    changed dfs.py

--- dfs.py
+++ dfs.py
@@ -1,28 +1,36 @@
 def add_edge(gr, u, v):
     if u not in gr:
-        gr[u] = []
+        graph[u] = []
     if v not in gr:
-        gr[v] = []
+        graph[v] = []
     gr[u].append(v)
     gr[v].append(u)
 
 
-def dfs(gr, start, visited=None):
+def dfs(gr, current, destination, visited=None, depth=0):
     if visited is None:
         visited = set()
-    visited.add(start)
-    print(start, end=" ")
-    for next_node in gr[start]:
+    if current == destination:
+        return True, depth
+    visited.add(current)
+    for next_node in graph[current]:
         if next_node not in visited:
-            dfs(gr, next_node, visited)
-    return
+            fnd, d = dfs(gr, next_node, destination, visited, depth+1)
+            if fnd:
+                return True, d
+    return False, 0
 
-# Пример создания графа и вызова функции обхода
+# Пример использования
 graph = {}
 add_edge(graph, 4, 2)
 add_edge(graph, 1, 3)
-add_edge(graph, 2, 4)  # Это ребро уже добавлено из предыдущего добавления, но повтор не влияет
+add_edge(graph, 2, 4)
 
-start_vertex = 1
-print("Путь обхода: ")
-dfs(graph, start_vertex)
+start_vertex = 2
+end_vertex = 4
+found, path_length = dfs(graph, start_vertex, end_vertex)
+
+if found:
+    print(f"Длина пути от {start_vertex} до {end_vertex}: {path_length}")
+else:
+    print(f"Путь от {start_vertex} до {end_vertex} не найден.")
